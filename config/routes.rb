4a0cc7edfab95da5c06a3f90adb715e26bbf3dc6
José Neto <joseneto002@gmail.com>
2023-09-05 09:43:52 -0300

    db creation and routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items do
+      resources :groups
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
